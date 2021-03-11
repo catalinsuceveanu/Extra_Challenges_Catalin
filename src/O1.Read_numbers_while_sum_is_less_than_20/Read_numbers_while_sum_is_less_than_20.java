fdcd6b770e18aac4a02fcf180547ed60b745b311
catalinsuceveanu <79462999+catalinsuceveanu@users.noreply.github.com>
2021-03-11 22:50:17 +0200

    Read typed inputs until the sum is 20 or over

---
+++ src/O1.Read_numbers_while_sum_is_less_than_20/Read_numbers_while_sum_is_less_than_20.java
@@ -0,0 +1,16 @@
+package O1.Read_numbers_while_sum_is_less_than_20;
+import java.util.Scanner;
+
+public class Read_numbers_while_sum_is_less_than_20 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); //new Scanner called scanner to allow us to read typed inputs
+        double sum = 0;                           //a double to store all the values
+
+        while (sum < 20) {    // a loop to repeat instructions while sum is under 20
+            System.out.print("Type a number: ");
+            sum = sum + scanner.nextDouble();  // sum is adding each value we type in
+        }
+        System.out.println("The sum of all the typed numbers is: " + sum);
+    }
+}
